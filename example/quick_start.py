85690f94a0b5c42f005bdb473bc83c31142b2525
wevsty <ty@wevs.org>
2025-03-22 16:10:45 +0800

    update example script

--- example/quick_start.py
+++ example/quick_start.py
@@ -2,22 +2,29 @@ from serial import Serial
 
 from pych9329 import keyboard
 from pych9329 import mouse
-from pych9329.chip_command import get_manufacturer
-from pych9329.chip_command import get_product
-from pych9329.chip_command import get_serial_number
+from pych9329 import chip_command
 
 
 def main():
     ser = Serial("COM10", 9600, timeout=0.50)
 
+    # Receive the keyboard lock status
+    result, kb_status = keyboard.receive_indicator_status(ser)
+    if result:
+        # {'usb_connect_status': True, 'num_lock': True, 'caps_lock': False, 'scroll_lock': False}
+        print(kb_status)
+        # Automatically turn off caps_lock
+        if kb_status["caps_lock"]:
+            keyboard.click(ser, "caps_lock")
+
     keyboard.click(ser, "a")
     keyboard.click(ser, "A")
-    keyboard.send_text_data(ser, "\n")
-    keyboard.send_text_data(ser, "Hello World\n")
-    keyboard.send_text_data(ser, "abcdefghijklmnopqrstuvwxyz\n")
-    keyboard.send_text_data(ser, "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n")
-    keyboard.send_text_data(ser, "0123456789\n")
-    keyboard.send_text_data(ser, "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n")
+    keyboard.send_text(ser, "\n")
+    keyboard.send_text(ser, "Hello World\n")
+    keyboard.send_text(ser, "abcdefghijklmnopqrstuvwxyz\n")
+    keyboard.send_text(ser, "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n")
+    keyboard.send_text(ser, "0123456789\n")
+    keyboard.send_text(ser, "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n")
 
     mouse.move(ser, x=500, y=500)
     mouse.move(ser, x=50, y=50, relative_mode=True)
@@ -25,11 +32,11 @@ def main():
     mouse.click(ser, button_name="left")
 
     # 2019A152BB40
-    print(get_serial_number(ser))
+    print(chip_command.get_serial_number(ser))
     # WCH UART TO KB-MS_V1.8
-    print(get_product(ser))
+    print(chip_command.get_product(ser))
     # WWW.WCH.CN
-    print(get_manufacturer(ser))
+    print(chip_command.get_manufacturer(ser))
     ser.close()
 
 
